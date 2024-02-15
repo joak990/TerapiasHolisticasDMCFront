f62ba5305d08631aee25017f3a0b41b9b09d51c2
patrickmurayari <patrickmurayari03@gmail.com>
2024-02-15 13:01:24 -0300

    add PopUp

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -21,7 +21,7 @@ const Popup = ({ onClose }) => {
         <div className="bg-white p-8 rounded-lg ">
           <button className="absolute top-0 right-0 mt-2 mr-2 text-gray-600" onClick={handleClose}>×</button>
           <h2 className="text-xl text-center font-bold mb-4">¿Te está gustando nuestra página web?</h2>
-          <p className="text-sm mb-4">Suscríbete a nuestro newsletter para recibir las últimas actualizaciones.</p>
+          <p className="text-sm mb-4">Suscríbete a nuestro newsletter para recibir las últimas actualizacionesss .</p>
           <form>
             <input type="email" className="border border-gray-300 rounded px-4 py-2 w-full mb-4" placeholder="Tu correo electrónico" required />
             <div className="flex justify-center">
